12fd45c3dcc08a8f7ff5a261f986c44f4f437c23
Anton Shcherbakov <1mailanton@gmail.com>
2023-11-19 22:46:20 +0100

    add decorator pattern

--- src/main/kotlin/structual/Decorator.kt
+++ src/main/kotlin/structual/Decorator.kt
@@ -1,46 +1,72 @@
 package structual
 
 /**
- * Lets you attach new behaviours to objects
- * by placing these objects inside special wrapper objects
- * that contains the behaviour
+ * The Decorator pattern is a structural design pattern that allows behavior to be added
+ * to individual objects dynamically, without affecting the behavior of other objects of the same class.
+ * It is used to extend or enhance the functionality of objects at runtime.
+ *
+ *
  */
 
-fun main(args: Array<String>) {
-    val peanutMilkShake = PeanutButterMilkShake(ConcreteMilkShake())
-    peanutMilkShake.getTaste()
-    val bananaMilkShake = BananaMilkShake(ConcreteMilkShake())
-    bananaMilkShake.getTaste()
+
+// Usage example
+fun main() {
+    // Creating a base beverage (Espresso)
+    var beverage: Beverage = Espresso()
+    println("Base beverage: ${beverage.getDescription()} - Cost: $${beverage.cost()}")
+
+    // Decorating with Milk
+    beverage = Milk(beverage)
+    println("Decorated beverage: ${beverage.getDescription()} - Cost: $${beverage.cost()}")
+
+    // Decorating with Sugar
+    beverage = Sugar(beverage)
+    println("Decorated beverage: ${beverage.getDescription()} - Cost: $${beverage.cost()}")
 }
 
-interface MilkShake {
-    fun getTaste()
+// Component interface
+interface Beverage {
+    fun getDescription(): String
+    fun cost(): Double
 }
 
-class ConcreteMilkShake : MilkShake {
-    override fun getTaste() {
-        println("It’s milk !")
+// Concrete Component
+class Espresso : Beverage {
+    override fun getDescription(): String {
+        return "Espresso"
+    }
+
+    override fun cost(): Double {
+        return 1.99
     }
 }
 
-open class MilkShakeDecorator(private var milkShake: MilkShake) : MilkShake {
-    override fun getTaste() {
-        this.milkShake.getTaste()
+// Decorator (abstract class)
+abstract class CondimentDecorator(
+    private val beverage: Beverage
+) : Beverage {
+    abstract override fun getDescription(): String
+}
+
+// Concrete Decorators
+class Milk(
+    private val beverage: Beverage
+) : CondimentDecorator(beverage) {
+    override fun getDescription(): String {
+        return "${beverage.getDescription()}, Milk"
+    }
+
+    override fun cost(): Double {
+        return beverage.cost() + 0.5
     }
 }
 
-class BananaMilkShake(m: MilkShake) : MilkShakeDecorator(m) {
-    override fun getTaste() {
-        super.getTaste()
-        println(" Adding Banana flavor to the milk shake !")
-        println(" It’s Banana milk shake !")
+class Sugar(private val beverage: Beverage) : CondimentDecorator(beverage) {
+    override fun getDescription(): String {
+        return "${beverage.getDescription()}, Sugar"
     }
-}
 
-class PeanutButterMilkShake(m: MilkShake) : MilkShakeDecorator(m) {
-    override fun getTaste() {
-        super.getTaste()
-        println(" Adding Peanut butter flavor to the milk shake !");
-        println(" It’s Peanut butter milk shake !");
+    override fun cost(): Double {
+        return beverage.cost() + 0.2
     }
 }
